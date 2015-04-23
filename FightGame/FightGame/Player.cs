3f477da34acc7b0b0f9e29814131956deac8b5e6
samaranin <samaranin@inbox.ru>
2015-04-23 12:05:07 +0300

    created team and player

---
+++ FightGame/FightGame/Player.cs
@@ -0,0 +1,94 @@
+﻿namespace FightGame
+{
+    class Player
+    {
+        private string Name;
+        private int ID;
+        private int Strength;
+        private int Agility;
+        private int Intellect;
+        private double Endurance;
+        private int Clothes;
+        private int Weapon;
+        private double _fortune;
+
+
+        public Player(string name, int id, int str, int agl, int intel, double end, int cloth, int wpn, double ftn)
+        {
+            Name = name;
+            ID = id;
+            Strength = str;
+            Intellect = intel;
+            Agility = agl;
+            Endurance = end;
+            Clothes = cloth;
+            Weapon = wpn;
+            _fortune = ftn;
+        }
+
+        protected Player()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetPlayerName()
+        {
+            return Name;
+        }
+
+        public int GetPlayerId()
+        {
+            return ID;
+        }
+
+        public int GetPlayerStrength()
+        {
+            return Strength;
+        }
+
+        public int GetPlayerAgility()
+        {
+            return Agility;
+        }
+
+        public int GetPlayerIntellect()
+        {
+            return Intellect;
+        }
+
+        public double GetPlayerEndurance()
+        {
+            return Endurance;
+        }
+
+        public int GetPlayerClothes()
+        {
+            return Clothes;
+        }
+
+        public int GetPlayerWeapon()
+        {
+            return Weapon;
+        }
+
+        public double GetPlayerFortune()
+        {
+            return _fortune;
+        }
+
+        public double GetPlayerMight()
+        {
+            return ( ( (Strength + Agility + Intellect)*Endurance ) + Clothes + Weapon )*_fortune;
+        }
+
+        public void PlayerIsWinner()
+        {
+            _fortune += 0.1;
+        }
+
+        public void PlayerIsLoser()
+        {
+            _fortune -= 0.1;
+        }
+    }
+}
---
+++ FightGame/FightGame/Team.cs
@@ -0,0 +1,40 @@
+﻿namespace FightGame
+{
+    class Team: Player
+    {
+        private static readonly Player[] TeamPlayers = new Player[5];
+
+        public Team(Player[] teamPlayers)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                TeamPlayers[i] = teamPlayers[i];
+            }
+        }
+
+        public double GetTeamPower()
+        {
+            double power = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                power += TeamPlayers[i].GetPlayerMight();
+            }
+
+            return power;
+        }
+
+        public void TeamIsWinner(bool trophy)
+        {
+            if (trophy)
+            {
+                for (int i = 0; i < 5; i++)
+                    TeamPlayers[i].PlayerIsWinner();
+            }
+            else
+            {
+                for (int i = 0; i < 5; i++)
+                    TeamPlayers[i].PlayerIsLoser();
+            }
+        }
+    }
+}
