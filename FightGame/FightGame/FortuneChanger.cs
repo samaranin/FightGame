03c1ed730dfeb1204aca3f53653c06d1e0647c6a
samaranin <samaranin@inbox.ru>
2015-04-28 19:46:15 +0300

    project finished

--- FightGame/FightGame/FortuneChanger.cs
+++ FightGame/FightGame/FortuneChanger.cs
@@ -2,149 +2,102 @@
 
 namespace FightGame
 {
-<<<<<<< HEAD
-    //class to update and reset fortune values
-    abstract class FortuneChanger
-    {
-        //connection
-        static OleDbConnection _connection;
+	//class to update and reset fortune values
+	abstract class FortuneChanger
+	{
+		//connection
+		static OleDbConnection _connection;
 
-        //update fortune for winner
-        public static void ChangeFortune(Player player)
-        {
-            //creates new connection
-=======
-    abstract class FortuneChanger
-    {
-        static OleDbConnection _connection;
+		//update fortune for winner
+		public static void ChangeFortune(Player player)
+		{
+			//creates new connection
+			_connection = new OleDbConnection
+			{
+				ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=players.mdb"
+			};
+			_connection.Open();
 
-        public static void ChangeFortune(Player player)
-        {
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-            _connection = new OleDbConnection
-            {
-                ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=players.mdb"
-            };
-            _connection.Open();
+			//look down
+			SetNewFortune(player);
 
-<<<<<<< HEAD
-            //look down
-=======
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-            SetNewFortune(player);
+			_connection.Close();
+		}
 
-            _connection.Close();
-        }
+		//set new values
+		private static void SetNewFortune(Player player)
+		{
+			//command to update
+			var cmd = new OleDbCommand
+			{
+				CommandType = System.Data.CommandType.Text,
+				CommandText = "UPDATE PlayersInfo SET [Fortune] = @Fortune WHERE [ID] = @ID",
+				Connection = _connection
+			};
 
-<<<<<<< HEAD
-        //set new values
-        private static void SetNewFortune(Player player)
-        {
-            //command to update
-=======
-        private static void SetNewFortune(Player player)
-        {
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-            var cmd = new OleDbCommand
-            {
-                CommandType = System.Data.CommandType.Text,
-                CommandText = "UPDATE PlayersInfo SET [Fortune] = @Fortune WHERE [ID] = @ID",
-                Connection = _connection
-            };
-<<<<<<< HEAD
+			//set values
+			cmd.Parameters.AddWithValue("@Fortune", player.GetPlayerFortune());
+			cmd.Parameters.AddWithValue("@ID", player.GetPlayerId());
 
-            //set values
-            cmd.Parameters.AddWithValue("@Fortune", player.GetPlayerFortune());
-            cmd.Parameters.AddWithValue("@ID", player.GetPlayerId());
+			//execute command
+			cmd.ExecuteNonQuery();
+		}
 
-            //execute command
-            cmd.ExecuteNonQuery();
-        }
+		//reset fortune values from enother table
+		public static void ResetFortune()
+		{
+			//one more connection
+			_connection = new OleDbConnection
+			{
+				ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=players.mdb"
+			};
+			_connection.Open();
 
-        //reset fortune values from enother table
-        public static void ResetFortune()
-        {
-            //one more connection
-=======
-            cmd.Parameters.AddWithValue("@Fortune", player.GetPlayerFortune());
-            cmd.Parameters.AddWithValue("@ID", player.GetPlayerId());
+			//array for base fortune values
+			var baseFortune = new double[20];
 
-            cmd.ExecuteNonQuery();
-        }
+			//cycle for every player
+			for (var id = 1; id <= 20; id++)
+			{
+				//gets our base values
+				var command = new OleDbCommand
+				{
+					CommandText = "SELECT * FROM BaseFortune WHERE ID = " + id + ";",
+					Connection = _connection
+				};
 
-        public static void ResetFortune()
-        {
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-            _connection = new OleDbConnection
-            {
-                ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=players.mdb"
-            };
-            _connection.Open();
+				OleDbDataReader dataReader = command.ExecuteReader();
 
-<<<<<<< HEAD
-            //array for base fortune values
-            var baseFortune = new double[20];
+				//and fill array
+				if (dataReader != null && dataReader.HasRows)
+				{
+					while (dataReader.Read())
+					{
+						baseFortune[id -1] = (double)dataReader["BaseFortuneValue"];
+					}
+					dataReader.Close();
+				}
 
-            //cycle for every player
-            for (var id = 1; id <= 20; id++)
-            {
-                //gets our base values
-=======
-            var baseFortune = new double[20];
+				//then update main table
+				var cmd = new OleDbCommand
+				{
+					CommandType = System.Data.CommandType.Text,
+					CommandText = "UPDATE PlayersInfo SET [Fortune] = @Fortune WHERE [ID] = @ID",
+					Connection = _connection
 
-            for (var id = 1; id <= 20; id++)
-            {
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-                var command = new OleDbCommand
-                {
-                    CommandText = "SELECT * FROM BaseFortune WHERE ID = " + id + ";",
-                    Connection = _connection
-                };
+				};
 
-                OleDbDataReader dataReader = command.ExecuteReader();
+				//with this parameters
+				cmd.Parameters.AddWithValue("@Fortune", baseFortune[id - 1]);
+				cmd.Parameters.AddWithValue("@ID", id);
 
-<<<<<<< HEAD
-                //and fill array
-=======
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-                if (dataReader != null && dataReader.HasRows)
-                {
-                    while (dataReader.Read())
-                    {
-                        baseFortune[id -1] = (double)dataReader["BaseFortuneValue"];
-                    }
-                    dataReader.Close();
-                }
+				cmd.ExecuteNonQuery();
 
-<<<<<<< HEAD
-                //then update main table
-=======
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-                var cmd = new OleDbCommand
-                {
-                    CommandType = System.Data.CommandType.Text,
-                    CommandText = "UPDATE PlayersInfo SET [Fortune] = @Fortune WHERE [ID] = @ID",
-                    Connection = _connection
+				command.ExecuteNonQuery();
+			}
 
-                };
-<<<<<<< HEAD
-
-                //with this parameters
-=======
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-                cmd.Parameters.AddWithValue("@Fortune", baseFortune[id - 1]);
-                cmd.Parameters.AddWithValue("@ID", id);
-
-                cmd.ExecuteNonQuery();
-
-                command.ExecuteNonQuery();
-            }
-
-<<<<<<< HEAD
-            //and close connection
-=======
->>>>>>> b789f308a943844a24df9d19ea24370a8daed87e
-            _connection.Close();
-        }
-    }
+			//and close connection
+			_connection.Close();
+		}
+	}
 }
